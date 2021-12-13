c7288b4db3ece8ed3584d48931f8a20f4555c709
reptilefury <reptilefury@outlook.com>
2021-12-13 07:42:32 +0300

    RecycleView by coding with Mitch

---
+++ app/src/main/java/com/example/recyclerview_mitch/DataSource.kt
@@ -0,0 +1,67 @@
+package com.example.recyclerview_mitch
+
+import com.example.recyclerview_mitch.Models.BlogPost
+
+class  DataSource(){
+companion object{
+    fun createDataSet(){
+        val list = ArrayList<BlogPost>()
+         list.add(
+             BlogPost(
+             "Manchester United",
+             "The red devils have done it again, they thrashed the Noisy Neighbours 6 in the manchester derby!! would you believe that!!",
+             "https://e3.365dm.com/21/09/1600x900/skynews-cristiano-ronaldo-old-trafford_5508083.jpg?20210911004513",
+             "Reptilefury"
+         )
+         )
+        list.add(
+            BlogPost(
+                "Manchester United",
+                "The red devils have done it again, they thrashed the Noisy Neighbours 6 in the manchester derby!! would you believe that!!",
+                "https://e3.365dm.com/21/09/1600x900/skynews-cristiano-ronaldo-old-trafford_5508083.jpg?20210911004513",
+                "Reptilefury"
+            )
+        )
+        list.add(
+            BlogPost(
+                "Manchester United",
+                "The red devils have done it again, they thrashed the Noisy Neighbours 6 in the manchester derby!! would you believe that!!",
+                "https://e3.365dm.com/21/09/1600x900/skynews-cristiano-ronaldo-old-trafford_5508083.jpg?20210911004513",
+                "Reptilefury"
+            )
+        )
+        list.add(
+            BlogPost(
+                "Manchester United",
+                "The red devils have done it again, they thrashed the Noisy Neighbours 6 in the manchester derby!! would you believe that!!",
+                "https://e3.365dm.com/21/09/1600x900/skynews-cristiano-ronaldo-old-trafford_5508083.jpg?20210911004513",
+                "Reptilefury"
+            )
+        )
+        list.add(
+            BlogPost(
+                "Manchester United",
+                "The red devils have done it again, they thrashed the Noisy Neighbours 6 in the manchester derby!! would you believe that!!",
+                "https://e3.365dm.com/21/09/1600x900/skynews-cristiano-ronaldo-old-trafford_5508083.jpg?20210911004513",
+                "Reptilefury"
+            )
+        )
+        list.add(
+            BlogPost(
+                "Manchester United",
+                "The red devils have done it again, they thrashed the Noisy Neighbours 6 in the manchester derby!! would you believe that!!",
+                "https://e3.365dm.com/21/09/1600x900/skynews-cristiano-ronaldo-old-trafford_5508083.jpg?20210911004513",
+                "Reptilefury"
+            )
+        )
+        list.add(
+            BlogPost(
+                "Manchester United",
+                "The red devils have done it again, they thrashed the Noisy Neighbours 6 in the manchester derby!! would you believe that!!",
+                "https://e3.365dm.com/21/09/1600x900/skynews-cristiano-ronaldo-old-trafford_5508083.jpg?20210911004513",
+                "Reptilefury"
+            )
+        )
+    }
+}
+}
